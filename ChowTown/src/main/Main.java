ec12efe37d215608e8797312b05e2fac038ffbfd
Mahin Khan <Mahin Khan@192.168.1.5>
2019-12-07 08:24:04 -0500

    added manager start page

--- ChowTown/src/main/Main.java
+++ ChowTown/src/main/Main.java
@@ -1,11 +1,45 @@
 package main;
 
-public class Main {
+import java.sql.*;
 
+public class Main {
+	private static final String url = "jdbc:mysql://localhost:3306/chowtown";
+	private static final String user = "root";
+	private static final String password = "mahin";
+	private static Connection conn = null;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Login log;
-		log = new Login();
+		//Login log;
+		//log = new Login();
+		
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			func();
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public static void func() {
+		String query = "SELECT * FROM customers WHERE username = 'mahin'";
+		Statement stmt;
+		try {
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				System.out.println(rs.getString("username"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }

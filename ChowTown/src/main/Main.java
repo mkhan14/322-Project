b570b71964ab244c9d969b26b488a048175e3c32
risa <r.morishima@outlook.jp>
2019-12-10 16:00:28 -0500

    goodbye

--- ChowTown/src/main/Main.java
+++ ChowTown/src/main/Main.java
@@ -1,109 +1,109 @@
 package main;
-
-<<<<<<< HEAD
-import java.sql.*;
-
-import javax.swing.*;
-
-public class Main {
-	private static final String url = "jdbc:mysql://localhost:3306/chowtown";
-	private static final String user = "root";
-	private static final String password = "mahin";
-	private static Connection conn = null;
-	
-	
-	private static JFrame frame;
-	//private static JFrame frameAgain;
-	private static ManagerPage manager_page;
-	private static JPanel managerPage;
-	private static Info info;
-	private static Login login;
-	private static Manager manager;
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Login log;
-		//log = new Login();
-		
-		try {
-			conn = DriverManager.getConnection(url, user, password);
-			//func();
-			//init();
-			goToLogin();
-		} 
-		catch (SQLException e) {
-			e.printStackTrace();
-		} 
-	}
-	
-	public static void func() {
-		String query = "SELECT * FROM customers WHERE username = 'mahin'";
-		Statement stmt;
-		try {
-			stmt = conn.createStatement();
-			ResultSet rs = stmt.executeQuery(query);
-			while(rs.next()) {
-				System.out.println(rs.getString("username"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	//new
-	public static void init() {
-		
-		frame = new JFrame();
-		manager_page = new ManagerPage();
-		info = new Info();
-		managerPage = manager_page.createPage();
-		frame.add(managerPage);
-		frame.setSize(600,850);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	public static void goToManagerPage() {
-		frame.getContentPane().removeAll();
-		frame.repaint();
-		frame.revalidate();
-		frame.add(managerPage);
-		frame.setVisible(true);
-		frame.setSize(600, 850);
-	}
-	
-	public static void goToInfo(int infoID) {
-		frame.getContentPane().removeAll();
-		frame.repaint();
-		frame.revalidate();
-		frame.add(info.generateInfo(infoID, manager.getId()));
-		//frame.add(info.generateInfo(infoID, restID));
-		frame.setVisible(true);
-		frame.setSize(1000, 850);
-	}
-	
-	public static void goToLogin() {
-		if(manager == null) {
-			login = new Login();
-			login.setVisible(true);
-		}else if(manager.isLoggedIn()) {
-			JOptionPane.showMessageDialog(null,"Already logged in.");
-		}
-	}
-	
-	public static Connection getConnection() {
-		return conn;
-	}
-	
-	public static Manager getManager() {
-		return manager;
-	}
-	public static void setManager(Manager m) {
-		manager = m;
-	}
-
-=======
+//
+//<<<<<<< HEAD
+//import java.sql.*;
+//
+//import javax.swing.*;
+//
+//public class Main {
+//	private static final String url = "jdbc:mysql://localhost:3306/chowtown";
+//	private static final String user = "root";
+//	private static final String password = "mahin";
+//	private static Connection conn = null;
+//	
+//	
+//	private static JFrame frame;
+//	//private static JFrame frameAgain;
+//	private static ManagerPage manager_page;
+//	private static JPanel managerPage;
+//	private static Info info;
+//	private static Login login;
+//	private static Manager manager;
+//	
+//	
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		//Login log;
+//		//log = new Login();
+//		
+//		try {
+//			conn = DriverManager.getConnection(url, user, password);
+//			//func();
+//			//init();
+//			goToLogin();
+//		} 
+//		catch (SQLException e) {
+//			e.printStackTrace();
+//		} 
+//	}
+//	
+//	public static void func() {
+//		String query = "SELECT * FROM customers WHERE username = 'mahin'";
+//		Statement stmt;
+//		try {
+//			stmt = conn.createStatement();
+//			ResultSet rs = stmt.executeQuery(query);
+//			while(rs.next()) {
+//				System.out.println(rs.getString("username"));
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	//new
+//	public static void init() {
+//		
+//		frame = new JFrame();
+//		manager_page = new ManagerPage();
+//		info = new Info();
+//		managerPage = manager_page.createPage();
+//		frame.add(managerPage);
+//		frame.setSize(600,850);
+//		frame.setVisible(true);
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//	}
+//	
+//	public static void goToManagerPage() {
+//		frame.getContentPane().removeAll();
+//		frame.repaint();
+//		frame.revalidate();
+//		frame.add(managerPage);
+//		frame.setVisible(true);
+//		frame.setSize(600, 850);
+//	}
+//	
+//	public static void goToInfo(int infoID) {
+//		frame.getContentPane().removeAll();
+//		frame.repaint();
+//		frame.revalidate();
+//		frame.add(info.generateInfo(infoID, manager.getId()));
+//		//frame.add(info.generateInfo(infoID, restID));
+//		frame.setVisible(true);
+//		frame.setSize(1000, 850);
+//	}
+//	
+//	public static void goToLogin() {
+//		if(manager == null) {
+//			login = new Login();
+//			login.setVisible(true);
+//		}else if(manager.isLoggedIn()) {
+//			JOptionPane.showMessageDialog(null,"Already logged in.");
+//		}
+//	}
+//	
+//	public static Connection getConnection() {
+//		return conn;
+//	}
+//	
+//	public static Manager getManager() {
+//		return manager;
+//	}
+//	public static void setManager(Manager m) {
+//		manager = m;
+//	}
+//
+//=======
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -211,5 +211,5 @@ public class Main {
 		user = c;
 	}
 	
->>>>>>> refs/remotes/origin/risa
+//>>>>>>> refs/remotes/origin/risa
 }

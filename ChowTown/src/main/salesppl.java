6a6c57f6b76348920bbe78fb3227dbad03ceffda
Wilson <wilsonlin9506@gmail.com>
2019-12-11 17:10:32 -0500

    sales-my account

--- ChowTown/src/main/salesppl.java
+++ ChowTown/src/main/salesppl.java
@@ -39,7 +39,13 @@ public class salesppl {
 	private static Connection conn = null;
 	private int selectedRow;
 	private String[][] menu_val;
-	private int index = 0;
+	private int index = 0, ck_id = 12;
+	private JTextField txtSalary;
+	private JTextField txtName;
+	private JTextField txtID;
+	private JTextField txtRating;
+	private JTextField txtRestaurant;
+	private JTextField txtTitle;
 	
 	/**
 	 * Launch the application.
@@ -92,8 +98,137 @@ public class salesppl {
 		JPanel Supplies = new JPanel();
 		parent.setLayout(new CardLayout(0, 0));
 		
+		JPanel Blank = new JPanel();
+		parent.add(Blank, "name_6697048408900");
+		
 		JPanel MyAccount = new JPanel();
 		parent.add(MyAccount, "name_313985598329700");
+		
+		JLabel label = new JLabel("ID :");
+		label.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel label_1 = new JLabel("Name :");
+		label_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JLabel label_2 = new JLabel("Salary :");
+		label_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		txtSalary = new JTextField();
+		txtSalary.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtSalary.setEditable(false);
+		txtSalary.setColumns(10);
+		
+		JLabel label_3 = new JLabel("Rating :");
+		label_3.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		txtName = new JTextField();
+		txtName.setEditable(false);
+		txtName.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtName.setColumns(10);
+		
+		JLabel label_4 = new JLabel("Title :");
+		label_4.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		txtID = new JTextField();
+		txtID.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtID.setEditable(false);
+		txtID.setColumns(10);
+		
+		JLabel label_5 = new JLabel("Restaurant :");
+		label_5.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		txtRating = new JTextField();
+		txtRating.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtRating.setEditable(false);
+		txtRating.setColumns(10);
+		
+		txtRestaurant = new JTextField();
+		txtRestaurant.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtRestaurant.setEditable(false);
+		txtRestaurant.setColumns(10);
+		
+		txtTitle = new JTextField();
+		txtTitle.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtTitle.setEditable(false);
+		txtTitle.setColumns(10);
+		
+		JLabel label_6 = new JLabel("My Account");
+		label_6.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		GroupLayout gl_MyAccount = new GroupLayout(MyAccount);
+		gl_MyAccount.setHorizontalGroup(
+			gl_MyAccount.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_MyAccount.createSequentialGroup()
+					.addGap(62)
+					.addGroup(gl_MyAccount.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addGap(200)
+							.addComponent(label_6, GroupLayout.PREFERRED_SIZE, 92, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addGap(2)
+							.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 52, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(txtName, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
+							.addGap(81)
+							.addComponent(label_4, GroupLayout.PREFERRED_SIZE, 42, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(txtTitle, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addGap(26)
+							.addComponent(label, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(txtID, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
+							.addGap(37)
+							.addComponent(label_5, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(txtRestaurant, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addComponent(label_2, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE)
+							.addGap(12)
+							.addComponent(txtSalary, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)
+							.addGap(67)
+							.addComponent(label_3)
+							.addGap(12)
+							.addComponent(txtRating, GroupLayout.PREFERRED_SIZE, 146, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(101, Short.MAX_VALUE))
+		);
+		gl_MyAccount.setVerticalGroup(
+			gl_MyAccount.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_MyAccount.createSequentialGroup()
+					.addGap(39)
+					.addComponent(label_6, GroupLayout.PREFERRED_SIZE, 22, GroupLayout.PREFERRED_SIZE)
+					.addGap(25)
+					.addGroup(gl_MyAccount.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addGap(6)
+							.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtName, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addGap(6)
+							.addComponent(label_4, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtTitle, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE))
+					.addGap(41)
+					.addGroup(gl_MyAccount.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addGap(6)
+							.addComponent(label, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtID, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addGap(6)
+							.addComponent(label_5, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtRestaurant, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE))
+					.addGap(41)
+					.addGroup(gl_MyAccount.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addGap(3)
+							.addComponent(label_2, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtSalary, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_MyAccount.createSequentialGroup()
+							.addGap(3)
+							.addComponent(label_3, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE))
+						.addComponent(txtRating, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(57, Short.MAX_VALUE))
+		);
+		MyAccount.setLayout(gl_MyAccount);
 		parent.add(Supplies, "name_313985649504000");
 		
 		JScrollPane supply_scrollPane = new JScrollPane();
@@ -199,13 +334,45 @@ public class salesppl {
 		
 		JButton btnLogOut = new JButton("Log out");
 		btnLogOut.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		
+		JButton btnMyAccount = new JButton("My Account");
+		btnMyAccount.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String[] values = myAccount();
+				parent.removeAll();		
+				txtName.setText(values[0]);
+				txtTitle.setText("Sales");
+				txtID.setText(values[2]);
+				switch(Integer.parseInt(values[3])) {
+					case 0:
+						txtRestaurant.setText("Panda Express");
+						break;
+					case 1:
+						txtRestaurant.setText("Sakura");
+						break;
+					case 2:
+						txtRestaurant.setText("Masala Cafe");
+						break;
+					default:
+						txtRestaurant.setText("Unknown");
+				}
+				txtSalary.setText(values[4]);
+				txtRating.setText(values[5]);
+				parent.add(MyAccount);
+				parent.repaint();
+				parent.revalidate();
+			}
+		});
+		btnMyAccount.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		GroupLayout gl_menu = new GroupLayout(menu);
 		gl_menu.setHorizontalGroup(
 			gl_menu.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_menu.createSequentialGroup()
-					.addGap(102)
+					.addGap(37)
 					.addComponent(btnVieweditSupplies, GroupLayout.PREFERRED_SIZE, 222, GroupLayout.PREFERRED_SIZE)
-					.addGap(151)
+					.addGap(23)
+					.addComponent(btnMyAccount, GroupLayout.PREFERRED_SIZE, 166, GroupLayout.PREFERRED_SIZE)
+					.addGap(27)
 					.addComponent(btnLogOut, GroupLayout.PREFERRED_SIZE, 112, GroupLayout.PREFERRED_SIZE)
 					.addContainerGap(69, Short.MAX_VALUE))
 		);
@@ -213,10 +380,17 @@ public class salesppl {
 			gl_menu.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_menu.createSequentialGroup()
 					.addGap(6)
-					.addGroup(gl_menu.createParallelGroup(Alignment.BASELINE)
-						.addComponent(btnLogOut, GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE)
-						.addComponent(btnVieweditSupplies, GroupLayout.DEFAULT_SIZE, 43, Short.MAX_VALUE))
+					.addComponent(btnLogOut, GroupLayout.DEFAULT_SIZE, 70, Short.MAX_VALUE)
 					.addGap(7))
+				.addGroup(Alignment.TRAILING, gl_menu.createSequentialGroup()
+					.addGroup(gl_menu.createParallelGroup(Alignment.TRAILING)
+						.addGroup(Alignment.LEADING, gl_menu.createSequentialGroup()
+							.addGap(25)
+							.addComponent(btnMyAccount, GroupLayout.DEFAULT_SIZE, 39, Short.MAX_VALUE))
+						.addGroup(gl_menu.createSequentialGroup()
+							.addGap(21)
+							.addComponent(btnVieweditSupplies, GroupLayout.DEFAULT_SIZE, 43, Short.MAX_VALUE)))
+					.addGap(19))
 		);
 		menu.setLayout(gl_menu);
 		frame.getContentPane().setLayout(groupLayout);
@@ -263,5 +437,22 @@ public class salesppl {
     		}
 		}
 		supply.setModel(refresh_supply());
+	}
+	public String[] myAccount() {
+		String[] result = new String[6];
+		String query = "select name, job_title, id, rest_id, salary, avg_rating from employees where id = " + ck_id;
+		Statement stmt;
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+			stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while(rs.next()) {
+				String[] value = {rs.getString("name"), rs.getString("job_title"), rs.getString("id"), rs.getString("rest_id"), rs.getString("salary"), rs.getString("avg_rating")};
+				result = value;
+			}			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 }

2294750604f2fb58837aaac5f6179b791d97fe52
Mahin Khan <Mahin Khan@192.168.94.132>
2019-12-10 19:42:52 -0500

    added mahin managerpage class

---
+++ ChowTown/src/main/ManagerPage.java
@@ -0,0 +1,85 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ManagerPage {
+
+	private static Manager manager;
+	
+	final static String[] rest = {"Panda Express", "Sakura", "Masala Cafe"};
+	
+	public JPanel createPage() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(4,1));
+		
+		if(Main.getManager().getId() == 0)
+			panel.setBackground(new Color(255, 255, 200));
+		if(Main.getManager().getId() == 1)
+			panel.setBackground(new Color(255, 230, 245));
+		if(Main.getManager().getId() == 2)
+			panel.setBackground(new Color(240, 255, 220));
+		
+		JLabel pageName = new JLabel(rest[Main.getManager().getId()] + "'s" + " Manager Page");
+		pageName.setFont(new Font("monospaced", Font.PLAIN, 28));
+		pageName.setHorizontalAlignment(JLabel.CENTER);
+		pageName.setVerticalAlignment(JLabel.CENTER);
+		panel.add(pageName);
+		
+		if(Main.getManager().getId() == 0) {
+			ImageIcon icon = new ImageIcon("images/China.png");
+			JLabel img = new JLabel(icon);
+			panel.add(img);
+		}
+		if(Main.getManager().getId() == 1) {
+			ImageIcon icon = new ImageIcon("images/Japan.png");
+			JLabel img = new JLabel(icon);
+			panel.add(img);
+		}
+		if(Main.getManager().getId() == 2) {
+			ImageIcon icon = new ImageIcon("images/India.png");
+			JLabel img = new JLabel(icon);
+			panel.add(img);
+		}
+		
+		
+		JButton customerBtn = new JButton("View Customer Info");
+		customerBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Main.goToInfo(Info.CI);
+			}
+			
+		});
+	   panel.add(customerBtn);
+	   
+	   
+	   JButton employeeBtn = new JButton("View Employee Info");
+	   employeeBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Main.goToInfo(Info.EI);
+			}
+			
+		});
+	   panel.add(employeeBtn);
+	   
+	   //JButton orderBtn = new JButton("View Order Info");
+	   //panel.add(orderBtn);
+	   JButton logoutBtn = new JButton("Logout");
+	   panel.add(logoutBtn);
+	   
+	   return panel;
+	}
+
+}

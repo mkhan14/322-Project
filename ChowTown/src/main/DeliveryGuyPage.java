e8feb509d750da50c1012fa44d135803ec27f18f
sumaiyamahin <snawarmahin@gmail.com>
2019-12-10 22:15:53 -0500

    afdabifbh

--- ChowTown/src/main/DeliveryGuyPage.java
+++ ChowTown/src/main/DeliveryGuyPage.java
@@ -1,7 +1,32 @@
 package main;
 
-public class DeliveryGuyPage {
-	
-	//private static Delivery_Guy 
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class DeliveryGuyPage {
+	public JPanel mainpage() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(2,2));
+		
+		JLabel name = new JLabel("Delivery Person's Page");
+		name.setFont(new Font("monospaced", Font.PLAIN, 28));
+		name.setHorizontalAlignment(JLabel.CENTER);
+		name.setVerticalAlignment(JLabel.CENTER);
+		panel.add(name);
+		
+		JButton bidButton = new JButton("BID");
+		bidButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPrformed(ActionEvent e) {
+				
+			}
+		})
+	}
+	
 }
--- ChowTown/src/main/Main.java
+++ ChowTown/src/main/Main.java
@@ -1,7 +1,21 @@
 package main;
 
-public class Main {
+import java.sql.*;
 
+import javax.swing.*;
+
+public class Main {
+	private static final String url = "jdbc:mysql://localhost:3306/chowtown";
+	private static final String user = "root";
+	private static final String password = "newyearseve1998";
+	private static Connection conn = null;
+	
+	
+	private static JFrame frame;
+	private static DeliveryGuyPage manager_page;
+	private static JPanel managerPage;
+	//private static Login login;
+	private static DeliveryGuy deliveryguy;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
